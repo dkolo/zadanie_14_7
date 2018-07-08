fe26b61e1e0e5b4d1186f905e581c02e69275e6f
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-08 10:01:19 +0200

    Remove commented React script

--- components/Contact.js
+++ components/Contact.js
@@ -17,20 +17,6 @@ var Contact = React.createClass({
                     {this.props.item.email}
                 </a>
             </div>
-
-            
-            /* /*React.createElement('div', { className: 'contactItem' },
-                React.createElement('img', {
-                    className: 'contactImage',
-                    src: 'http://icons.veryicon.com/png/System/Long%20Shadow%20Polygon/contact%202.png'
-                }),
-                React.createElement('p', { className: 'contactLabel' }, 'Imię: ' + this.props.item.firstName),
-                React.createElement('p', { className: 'contactLabel' }, 'Nazwisko: ' + this.props.item.lastName),
-                React.createElement('a', { className: 'contactEmail', href: 'mailto:' + this.props.item.email },
-                    this.props.item.email
-                )
-            )
-            */
         )
     },
 });
