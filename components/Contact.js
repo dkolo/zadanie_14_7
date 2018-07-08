911651473001a433dd4c1f256ecb109e8cfc183f
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-08 09:53:34 +0200

    Change Contact.js script into JSX

--- components/Contact.js
+++ components/Contact.js
@@ -5,7 +5,21 @@ var Contact = React.createClass({
 
     render: function() {
         return (
-            React.createElement('div', { className: 'contactItem' },
+            <div className={'contactItem'}>
+                <img className={'contactImage'} src={'http://icons.veryicon.com/png/System/Long%20Shadow%20Polygon/contact%202.png'}/>
+                <p className={'contactLabel'}>
+                    Imię: {this.props.item.firstName}
+                </p>
+                <p className={'contactLabel'}>
+                    Imię: {this.props.item.lastName}
+                </p>
+                <a href={'mailto: ' + this.props.item.email}>
+                    {this.props.item.email}
+                </a>
+            </div>
+
+            
+            /* /*React.createElement('div', { className: 'contactItem' },
                 React.createElement('img', {
                     className: 'contactImage',
                     src: 'http://icons.veryicon.com/png/System/Long%20Shadow%20Polygon/contact%202.png'
@@ -16,6 +30,7 @@ var Contact = React.createClass({
                     this.props.item.email
                 )
             )
+            */
         )
     },
 });
